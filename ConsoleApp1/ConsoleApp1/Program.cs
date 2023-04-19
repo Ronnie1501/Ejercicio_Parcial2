417b0381e031550dc28bb6e0871f3d4a3eab97e0
Ronnie1501 <124007445+Ronnie1501@users.noreply.github.com>
2023-04-18 20:54:04 -0600

    ejercicio

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿//int dato1 = 16;
+//String dato2 = "25g";
+//int resultado = dato1 + Convert.ToInt32(dato2);
+
+int dato1 = 16;
+String dato2 = "25g";
+
+try
+{
+    int resultado = dato1 + Convert.ToInt32(dato2);
+    Console.WriteLine(resultado);
+}
+catch (Exception)
+{
+    Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
+}
